f62a924d1bd60520d918614e3a3323699dd6fc12
BrianLichliter <brian.lichliter@gmail.com>
2014-01-19 11:40:05 -0600

    changed theme

--- js/design.js
+++ js/design.js
@@ -6,7 +6,7 @@ if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
   target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
   if (target.length) {
     $('html,body').animate({
-      scrollTop: (target.offset().top - 80)
+      scrollTop: (target.offset().top - 75)
     }, 500);
     return false;
   }
